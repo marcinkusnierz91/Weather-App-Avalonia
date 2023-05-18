f855dd97493da987e058432dc93e14f4fbb03f1f
Marcin Kuśnierz <marcinkusnierz91@gmail.com>
2023-05-18 20:01:48 +0200

    first commit

---
+++ WeatherApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace WeatherApp.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Elo mordo!";
+}
---
+++ WeatherApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace WeatherApp.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
