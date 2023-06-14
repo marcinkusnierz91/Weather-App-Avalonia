cd32be4ca108126aabc991c1d4865c387d53fac8
Marcin Kuśnierz <marcinkusnierz91@gmail.com>
2023-06-14 22:59:25 +0200

    textbox added

--- WeatherApp/ViewModels/MainWindowViewModel.cs
+++ WeatherApp/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,61 @@
-﻿namespace WeatherApp.ViewModels;
+﻿using System.ComponentModel;
+using Avalonia.Controls;
+using Avalonia.Input;
 
-public class MainWindowViewModel : ViewModelBase
+
+namespace WeatherApp.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
 {
-    public string Greeting => "Welcome to the ugliest weather app ever!!";
+    private string greeting = "Welcome to the ugliest weather app ever!!";
+    private string enteredText;
+    private string displayText;
+
+    public string Greeting
+    {
+        get { return greeting; }
+        set
+        {
+            if (greeting != value)
+            {
+                greeting = value;
+                OnPropertyChanged(nameof(Greeting));
+            }
+        }
+    }
+
+    public string EnteredText
+    {
+        get { return enteredText; }
+        set
+        {
+            if (enteredText != value)
+            {
+                enteredText = value;
+                OnPropertyChanged(nameof(EnteredText));
+            }
+        }
+    }
+
+    public string DisplayText
+    {
+        get { return displayText; }
+        set
+        {
+            if (displayText != value)
+            {
+                displayText = value;
+                OnPropertyChanged(nameof(DisplayText));
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    
     
 }
--- WeatherApp/Views/MainWindow.axaml.cs
+++ WeatherApp/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using WeatherApp.ViewModels;
 
 namespace WeatherApp.Views;
 
@@ -8,4 +11,24 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+
+        var textBox = this.FindControl<TextBox>("TextBox");
+        textBox.KeyDown += HandleKeyDown;
+    }
+
+    private void HandleKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            var textBox = (TextBox)sender;
+            var viewModel = (MainWindowViewModel)DataContext;
+            viewModel.DisplayText = textBox.Text;
+        }
+    }
+
+  
 }
