7f489a5267f5e833a48754d161be8d9cc6f650f9
Marcin Kuśnierz <marcinkusnierz91@gmail.com>
2023-06-15 16:19:18 +0200

    api connection added

---
+++ WeatherApp/Models/WeatherData.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace WeatherApp.Models;
+
+public class MainData
+{
+    [JsonPropertyName("temp")]
+    public float Temp { get; set; }
+}
+
+public class WeatherInfo
+{
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+}
+
+public class WeatherData
+{
+    [JsonPropertyName("main")]
+    public MainData Main { get; set; }
+
+    [JsonPropertyName("weather")]
+    public List<WeatherInfo> Weather { get; set; }
+}
--- WeatherApp/Views/MainWindow.axaml.cs
+++ WeatherApp/Views/MainWindow.axaml.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Input;
-using Avalonia.Markup.Xaml;
 using WeatherApp.ViewModels;
 
+using WeatherApp.Models;
+
 namespace WeatherApp.Views;
+
 
 public partial class MainWindow : Window
 {
@@ -11,24 +17,44 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+    
+    private const string apiKey = "69d0f64763a71447797501363435451b";
+    private const string apiUrl = "https://api.openweathermap.org/data/2.5/weather";
 
-    private void InitializeComponent()
+    private readonly HttpClient httpClient = new HttpClient();
+
+    // ...
+
+    private async Task GetWeatherData(string city)
     {
-        AvaloniaXamlLoader.Load(this);
+        try
+        {
+            var url = $"{apiUrl}?q={city}&appid={apiKey}";
+            var response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
 
-        var textBox = this.FindControl<TextBox>("TextBox");
-        textBox.KeyDown += HandleKeyDown;
+            var json = await response.Content.ReadAsStringAsync();
+            var weatherData = JsonSerializer.Deserialize<WeatherData>(json);
+
+            var viewModel = (MainWindowViewModel)DataContext;
+            
+            float temperatureCelsius = weatherData.Main.Temp - 273.15f;
+            viewModel.DisplayText = $"Temperature: {temperatureCelsius:F1}°C, Weather: {weatherData.Weather[0].Description}";
+            // viewModel.DisplayText = $"Temperature: {weatherData.Main.Temp}°C, Weather: {weatherData.Weather[0].Description}";
+        }
+        catch (Exception ex)
+        {
+            // Obsłuż błędy związane z żądaniem HTTP lub przetwarzaniem JSON.
+            Console.WriteLine(ex.Message);
+        }
     }
 
-    private void HandleKeyDown(object sender, KeyEventArgs e)
+    private async void HandleKeyDown(object sender, KeyEventArgs e)
     {
         if (e.Key == Key.Enter)
         {
             var textBox = (TextBox)sender;
-            var viewModel = (MainWindowViewModel)DataContext;
-            viewModel.DisplayText = textBox.Text;
+            await GetWeatherData(textBox.Text);
         }
     }
-
-  
 }
